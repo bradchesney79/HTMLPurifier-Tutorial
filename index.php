97d5fc283055ce60003a7f9222074434dab2ed3f
BradChesney79 <BradChesney79@gmail.com>
2012-06-11 08:14:13 -0400

    Added index file draft

---
+++ index.php
@@ -0,0 +1,45 @@
+<html>
+<head>
+<title>HTMLPurifier Tutorial Index</title>
+</head>
+<body>
+<h1>HTMLPurifier Tutorial</h1>
+<p>HTMLPurifier is a filter library built by Edward Z. Yang. It does a number of things. Specifically, HTMLPurifier can change the character set of web content, prevent XSS, and modify content for Document Type Definition (DTD) compliance-- and more. Simply, it takes a chunk of your web page if not all of it and makes it more web browser friendly, safer for your systems, safer for the visitor, or all of the above.</p>
+
+<h2>Index</h2>
+
+<ul>
+  <li>Version 4.4.0</li>
+  <li>
+    <ul>
+      <li>Installation</li>
+      <li>
+        <ul>
+          <li>Normal</li>
+          <li>Standalone</li>
+        </ul>
+      </li>
+      <li>Configurations</li>
+      <li>
+        <ul>
+          <li>Basic</li>
+          <li>
+            <ul>
+              <li>Bare Minimum</li>
+            </ul>
+          </li>
+          <li>Advanced</li>
+        </ul>
+      </li>
+      <li>Usage Examples</li>
+      <li>
+        <ul>
+          <li>User Input Sanitization</li>
+        </ul>
+      </li>
+    </ul>
+  </li>
+</ul>
+
+</body>
+</html>
